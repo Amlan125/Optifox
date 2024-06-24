457c1eff1c71c57054ea9c3fa1cdec4c578127ac
Prarthana <prarthana.gogoi@fau.de>
2024-06-24 22:49:42 +0200

    updated tables

--- frontendUI/my-app/src/components/PatientPage.tsx
+++ frontendUI/my-app/src/components/PatientPage.tsx
@@ -1,47 +1,25 @@
+
+// PatientPage.tsx
+
 "use client";
 
 import React from 'react';
 import AppBarComponent from './AppBarComponent';
-import ReadmissionComponent from './readmission_rate';
-import MortalityRate from './mortality_rate';
-import { useThemeContext } from '../context/ThemeContext';
+import PatientTable from './PatientTable';
 
 const PatientPage: React.FC = () => {
-  const { isDarkMode } = useThemeContext(); // Fetch isDarkMode from context or state
-
-  const readmissionData = [
-    { date: '03/06/2024', readmissionLikelihood: 16 },
-    { date: '02/06/2024', readmissionLikelihood: 15 },
-    { date: '01/06/2024', readmissionLikelihood: 9 },
-  ];
-
-  const mortalityData = [
-    { date: '03/06/2024', mortalityRate: 8 },
-    { date: '02/06/2024', mortalityRate: 11 },
-    { date: '01/06/2024', mortalityRate: 10 },
-  ];
-
   return (
     <div>
-      <main className={isDarkMode ? 'bg-blue-950' : 'bg-blue-200'} style={{ display: 'grid', gridTemplateColumns: 'repeat(4, 1fr)', gridTemplateRows: 'auto repeat(4, 1fr)', gap: '10px' }}>
-        <div style={{ gridColumn: '1 / 5', gridRow: '1' }}>
-          <AppBarComponent />
-        </div>
-        <div style={{ gridColumn: '1 / 3', gridRow: '2 / 4' }}></div>
-        <div style={{ gridColumn: '3 / 5', gridRow: '2 / 4' }}></div>
-
-        <div style={{ gridColumn: '1 / 2', gridRow: '4 / 6' }}>
-          <ReadmissionComponent data={readmissionData} isDarkMode={isDarkMode} />
-        </div>
-
-        <div style={{ gridColumn: '2 / 3', gridRow: '4 / 6' }}>
-          <MortalityRate data={mortalityData} />
-        </div>
-
-        <div style={{ gridColumn: '3 / 5', gridRow: '4 / 6' }}></div>
-      </main>
+      <AppBarComponent />
+      <div style={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'flex-start', height: '80vh', paddingLeft: '10px', paddingTop: '20px' }}>
+        <PatientTable />
+      </div>
+      {/* Add your patient page content here */}
     </div>
   );
 };
 
 export default PatientPage;
+
+
+// </div><div style={{ display: 'flex', justifyContent: 'left', alignItems: 'center', height: '80vh' }}></div>
---
+++ frontendUI/my-app/src/components/PatientTable.tsx
@@ -0,0 +1,66 @@
+// PatientTable.tsx
+
+import React from 'react';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Grid } from '@mui/material';
+
+const Readmissiondata = [
+  { date: '03/06/2024', readmissionLikelihood: true },
+  { date: '02/06/2024', readmissionLikelihood: true },
+  { date: '01/06/2024', readmissionLikelihood: false },
+];
+
+const Mortalitydata = [
+  { date: '03/06/2024', mortalityLikelihood: true },
+  { date: '02/06/2024', mortalityLikelihood: true },
+  { date: '01/06/2024', mortalityLikelihood: false },
+];
+
+const PatientTable: React.FC = () => {
+  return (
+    <Grid container spacing={15} justifyContent="center">
+      <Grid item xs={6}>
+        <TableContainer component={Paper} style={{ marginTop: '20px' }}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Date</TableCell>
+                <TableCell>Readmission Likelihood</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {Readmissiondata.map((row, index) => (
+                <TableRow key={index} style={{ backgroundColor: row.readmissionLikelihood ? 'red' : 'green' }}>
+                  <TableCell>{row.date}</TableCell>
+                  <TableCell>{row.readmissionLikelihood.toString()}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Grid>
+
+      <Grid item xs={6}>
+        <TableContainer component={Paper} style={{ marginTop: '20px' }}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Date</TableCell>
+                <TableCell>Mortality Likelihood</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {Mortalitydata.map((row, index) => (
+                <TableRow key={index} style={{ backgroundColor: row.mortalityLikelihood ? 'red' : 'green' }}>
+                  <TableCell>{row.date}</TableCell>
+                  <TableCell>{row.mortalityLikelihood.toString()}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default PatientTable;
