333f447e8393a08fd3e968a03a04de7e480bda2d
aswinbenny <aswinbenny030@gmail.com>
2024-06-25 16:16:51 +0200

    update the search bar component

--- frontendUI/my-app/src/components/SearchBar.tsx
+++ frontendUI/my-app/src/components/SearchBar.tsx
@@ -1,27 +1,32 @@
 // This file defines a reusable SearchBar component.
 import React, { useEffect, useState } from "react";
 
+// Define the props expected by the SearchBar component.
 type SearchBarProps = {
-  onSearch: (query: string) => void;
+  onSearch: (query: string) => void; // Function to be called when a search is performed.
 };
 
+// Define the style types for the SearchBar component.
 export type SearchStyle = {
-  form: string;
-  iconContainer: string;
-  icon: string;
-  iconPath: string;
-  input: string;
+  form: string; // Style for the form element.
+  iconContainer: string; // Style for the icon container.
+  icon: string; // Style for the icon itself.
+  input: string; // Style for the input element.
 };
 
+// The SearchBar functional component.
 const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState(""); // State to hold the current query.
+
+  // Handle changes to the input field.
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setQuery(event.target.value);
-    onSearch(event.target.value);
+    setQuery(event.target.value); // Update the query state with the new value.
+    onSearch(event.target.value); // Trigger the onSearch function passed as a prop.
   };
 
+  // Handle the form submission.
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+    event.preventDefault(); // Prevent the default form submission behavior.
   };
 
   const classNames: SearchStyle = {
@@ -29,7 +34,6 @@ const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
     iconContainer:
       "pointer-events-none absolute inset-y-0 left-[16px] flex items-center pr-3",
     icon: "w-[12px] h-[12px]",
-    iconPath: "stroke-[#64748B]",
     input:
       "w-full text-[12px] font-[500] leading-[20px] text-[#374151] outline-none placeholder:text-[#94A3B8]",
   };
@@ -50,7 +54,7 @@ const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
         >
           <path
             d="M10.5 10.5L8.34998 8.34998M9.5 5.5C9.5 7.70914 7.70914 9.5 5.5 9.5C3.29086 9.5 1.5 7.70914 1.5 5.5C1.5 3.29086 3.29086 1.5 5.5 1.5C7.70914 1.5 9.5 3.29086 9.5 5.5Z"
-            className={classNames.iconPath}
+            stroke="#64748B"
             strokeLinecap="round"
             strokeLinejoin="round"
           />
